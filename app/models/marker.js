8f9b1e91c447ccdf735a02f8c09034a5e988a830
greatermeans <gakouri@gmail.com>
2016-08-01 15:36:50 -0400

    everything

--- app/models/marker.js
+++
@@ -1,9 +0,0 @@
-const Marker = function() {
-    return class {
-      constructor(position, map, title)
-      this.position = position,
-      this.map = map,
-      this.title = title
-
-    }
-}
---
+++ app/models/theMap.js
@@ -0,0 +1,10 @@
+const Mapped = (function() {
+  return class {
+    constructor(center, zoom, markers) {
+      this.center = center
+      this.markers = markers
+      this.zoom = zoom
+      
+      }
+    }
+  }());
