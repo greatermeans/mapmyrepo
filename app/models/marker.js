5d94001332676aaa10042540235ccc5b186b1b93
Dan <dandanberry@gmail.com>
2016-08-01 14:23:47 -0400

    marker model fleshed out

---
+++ app/models/marker.js
@@ -0,0 +1,9 @@
+const Marker = function() {
+    return class {
+      constructor(position, map, title)
+      this.position = position,
+      this.map = map,
+      this.title = title
+
+    }
+}
