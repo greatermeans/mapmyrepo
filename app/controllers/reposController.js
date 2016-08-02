0ca031520f213833268faa4ae14ad262cd7d7ba4
davidtsai130 <davidtsai130@gmail.com>
2016-08-02 10:52:27 -0400

    hid repo boxes

--- app/controllers/reposController.js
+++ app/controllers/reposController.js
@@ -4,8 +4,10 @@ var reposController = (function () {
   function displayRepositories() {
   	var string = $("#repositories-template").html();
   	var template = Handlebars.compile(string);
-  	var repoList = template(store.repos)
-  	$('#repositories').html(repoList)
+  	var repoList1 = template(store.repos.slice(0, 9))
+    var repoList2 = template(store.repos.slice(10, 19))
+  	$('#repositories1').html(repoList1)
+    $('#repositories2').html(repoList2)
   }
 
   return {
